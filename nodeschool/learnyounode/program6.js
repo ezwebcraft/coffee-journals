84ef675e6edeeeef70a6b12f48202e8e9c49df2a
ezwebcraft <real8686@gmail.com>
2017-05-27 23:59:43 -0400

    feat: finish 6

--- nodeschool/learnyounode/program6.js
+++ nodeschool/learnyounode/program6.js
@@ -5,7 +5,7 @@ var dirname = process.argv[2];
 var ext = process.argv[3];
 
 module1(dirname, ext, function(err, files) {
-
+ for (i = 0; i < files.length; i++) {
     console.log(files[i]);
-
+}
 });
