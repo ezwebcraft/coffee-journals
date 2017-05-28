862d1e6612a03d244a63015cf7d84c80796901aa
ezwebcraft <real8686@gmail.com>
2017-05-27 23:44:45 -0400

    feat: add console and error checking

--- nodeschool/learnyounode/program6.js
+++ nodeschool/learnyounode/program6.js
@@ -1,4 +1,14 @@
+// add both argv
 var dir = process.argv[2]
 var ext = process.argv[3]
 
-require('./moduleDir')()
+require('./moduleDir')(dir, ext, function (err, data) {
+// add error checking
+  if (err) {
+		return console.error(err)
+	}
+// console out for each data
+	data.forEach(function (item) {
+		console.log(item);
+	})
+})
