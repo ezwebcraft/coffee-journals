ba386c5fcac316cee77dda054ea1a3a1135c3201
Elvin Ramirez <real8686@gmail.com>
2017-08-26 21:21:09 -0400

    feat: Passing Data With Props Recap

--- basic_react/react_contacts/reactnd-contacts-complete-starter-files-added/src/ListContacts.js
+++ basic_react/react_contacts/reactnd-contacts-complete-starter-files-added/src/ListContacts.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 
 class ListContacts extends Component {
 render(){
-//console.log('Props',this.props)
 
 return (
 	<ol className="contact-list"> 
 	{ this.props.contact.map((contact) =>(
- 	<li key={contact.id}>
+ 	<li key={contact.id} className='contact-list-item'>
 
-		{contact.name }
+		<div className='contact-avatar' style={{
+			backgroundImage:`url(${contact.avatarUrl})`	
+		}}/>
+		<div className='contact-details'>
+			<p>{contact.name}</p>
+			<p>{contact.email}</p>
+		</div>
+		<button className='contact-remove'>
+			remove	
+		</button>
 	</li>
 	))}		
 	</ol> 
