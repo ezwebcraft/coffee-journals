f09813ce7df74dcc34731bd64f596511ce1c4994
Elvin Ramirez <real8686@gmail.com>
2017-09-14 22:50:40 -0400

    feat: Route Component

--- basic_react/react_contacts/reactnd-contacts-complete-starter-files-added/src/App.js
+++ basic_react/react_contacts/reactnd-contacts-complete-starter-files-added/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom'
 import ListContacts from './ListContacts'
 import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
-    screen: 'list', // list, create
     contacts: []
   }
   componentDidMount() {
@@ -22,19 +22,14 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="app">
-      {this.state.screen === 'list' && (
-        <ListContacts
-          onDeleteContact={this.removeContact}
-          contacts={this.state.contacts}
-          onNavigate={() => {
-            this.setState({ screen: 'create' })
-           }}
-        />
-      )}
-      {this.state.screen === 'create' && (
-        <CreateContact/>
-      )}
+      <div>
+        <Route exact path='/' render={() => (
+          <ListContacts
+            onDeleteContact={this.removeContact}
+            contacts={this.state.contacts}
+          />
+        )}/>
+        <Route path='/create' component={CreateContact}/>
       </div>
     )
   }
