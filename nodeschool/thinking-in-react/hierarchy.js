82556fa2ad03f4f62c695717949058b50ae5739a
Elvin Ramirez <real8686@gmail.com>
2017-10-22 00:02:07 -0400

    feat: add components

--- nodeschool/thinking-in-react/hierarchy.js
+++ nodeschool/thinking-in-react/hierarchy.js
@@ -1,5 +1,19 @@
-class ProductRow extends React.Component {
+class ProductCategoryRow  extends React.Component {
 }
 
 class ProductRow extends React.Component {
 }
+
+class SearchBar extends React.Component {
+
+}
+class ProductTable extends React.Component {
+
+}
+class FilterableProductTable extends React.Component {
+
+}
+
+const PRODUCTS = []
+
+ReactDOM.render();
