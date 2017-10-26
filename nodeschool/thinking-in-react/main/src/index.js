289fb753450b26a5fad0bf26b3d0d70c871e9565
Elvin Ramirez <real8686@gmail.com>
2017-10-25 22:32:59 -0400

    feat: completed ProductRow

--- nodeschool/thinking-in-react/main/src/index.js
+++ nodeschool/thinking-in-react/main/src/index.js
@@ -8,7 +8,7 @@ class ProductCategoryRow  extends React.Component {
 		return(
 
 			<tr>
-			 <th colSpan='2'>
+			 <th colSpan="2">
 			 	{category}
 			 </th>
 			</tr>
@@ -20,28 +20,73 @@ class ProductCategoryRow  extends React.Component {
 class ProductRow extends React.Component {
 	render(){
 		const products = this.props.products;
-		const name = products.stocked ?
-		<span style = {{ color: 'red'}}>
+		const name = products.stocked ? 
+		products.name :
+		<span style = {{ color: 'red'}} >
 			{products.name}
 		</span>;
-
 
 	return(
 		<tr>
 			<td>{name}</td>
 			<td>products.price</td>
 		</tr>
-
 		);
 	}
 }
 
+class ProductTable extends React.Component {
+   render(){
+
+   	const row = [];
+   	let lastCategory = null;
+
+
+   	this.props.products.forEach((products) => {
+   		if(products.category !== lastCategory){
+   			rows.push(
+   			<ProductCategoryRowou
+   				category = {products.category}
+   				key={products.category} />
+   			);
+
+   		}
+   	
+
+   	rows.push(
+   			<ProductCategoryRowou
+   				category = {products.category}
+   				key={products.category} />
+
+
+   	);
+
+
+   });
+
+   return (
+   	<table>
+   	<thread>
+   	<tr>
+   	<th>Name</th>
+   	<th>Price</th>
+   	</tr>
+   	<tbody>{rows}</tbody>
+   	</thread>
+
+   	</table>
+
+
+   	);
+	}
+}
+
+
+
 class SearchBar extends React.Component {
 
 }
-class ProductTable extends React.Component {
 
-}
 class FilterableProductTable extends React.Component {
 render() {
     return (
