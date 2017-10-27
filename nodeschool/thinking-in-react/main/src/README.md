79d0f41802bdb12a2e6d97448ee413f80785ab7f
Elvin Ramirez <real8686@gmail.com>
2017-10-26 23:05:09 -0400

    feat: adding the ProductTable and it's child components; ProductRow and ProductCategoryRow

--- nodeschool/thinking-in-react/main/src/README.md
+++ nodeschool/thinking-in-react/main/src/README.md
@@ -9,3 +9,12 @@ we have five components in our simple app. We’ve italicized the data each comp
 - ProductTable (green): displays and filters the data collection based on user input
 - ProductCategoryRow (turquoise): displays a heading for each category
 - ProductRow (red): displays a row for each product
+
+
+### Main parts
+
+- Static version - step 1
+- Step 2 : Build static version in React
+
+![stage2](../stage-2.png)
+
--- nodeschool/thinking-in-react/main/src/solution.js
+++ nodeschool/thinking-in-react/main/src/solution.js
@@ -1,19 +1,38 @@
-    import React from 'react'                                                                                                                                                              
-                                                                                                                                                                                           
-    export const FilterableProductTable = React.createClass({                                                                                                                              
-        render() {                                                                                                                                                                         
-            return (                                                                                                                                                                       
-                <div></div>                                                                                                                                                                
-            )                                                                                                                                                                              
-        }                                                                                                                                                                                  
-    });      
+import React from 'react';
+
+export const SearchBar = React.createClass({
+    render() {
+        return (
+            <form>
+                <input type="search" placeholder="Search..."/>
+                <label>
+                    <input type="checkbox"/>
+                    Only show products in stock
+                </label>
+            </form>
+        );
+    },
+});
+
+export const FilterableProductTable = React.createClass({
+    render() {
+        return (
+            <SearchBar/>
+        );
+    },
+});
+
+export const ProductTable = React.createClass({
 
 
-   	export const SearchBar = React.createClass({                                                                                                                              
-        render() {                                                                                                                                                                         
-            return (                                                                                                                                                                       
-                 <SearchBar/>                                                                                                                                                           
-            )                                                                                                                                                                              
-        }                                                                                                                                                                                  
-    });      
+}):
 
+export const ProductRow = React.createClass({
+
+
+}):
+
+export const ProductCategoryRow = React.createClass({
+
+	
+}):
