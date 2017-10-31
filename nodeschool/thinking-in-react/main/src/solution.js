084a967f4b5088a47af0af55997de5a314ceb507
Elvin Ramirez <real8686@gmail.com>
2017-10-30 23:09:02 -0400

    feat: add product array

--- nodeschool/thinking-in-react/main/src/solution.js
+++ nodeschool/thinking-in-react/main/src/solution.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 export const SearchBar = React.createClass({
     render() {
@@ -97,6 +98,16 @@ export const ProductCategoryRow = React.createClass({
 
     
 });
+
+
+const PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
 
 
 ReactDOM.render(
